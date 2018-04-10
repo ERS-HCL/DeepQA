4e9a04ab441ae4fad52cbc1ff963c2d7c21280fd
Mohanraj <mohanraj4072@gmail.com>
2018-04-10 21:26:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+"""
+Main script. See README.md for more information
+
+Use python 3
+"""
+
+from chatbot import chatbot
+
+
+if __name__ == "__main__":
+    chatbot = chatbot.Chatbot()
+    chatbot.main()
